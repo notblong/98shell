80af1649d987f7da5b9a7c3e8198d24c1f227a3c
Long Pham <longpham.work23@gmail.com>
2023-10-21 12:57:04 +0700

    added react env vars

--- 98terminal/src/axios.ts
+++ 98terminal/src/axios.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const prodUrl = "https://lobster-app-5occx.ondigitalocean.app/";
 axios.defaults.baseURL =
-  // prodUrl ??
-  "http://127.0.0.1:3000";
+  import.meta.env.VITE_API_URLS ?? "http://127.0.0.1:3000";
 
 export default axios;
--- 98terminal/src/socket.ts
+++ 98terminal/src/socket.ts
@@ -1,8 +1,4 @@
 import socketClient from "socket.io-client";
 
-// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-const prodUrl = "https://lobster-app-5occx.ondigitalocean.app/";
-const URL =
-  // prodUrl ??
-  "http://127.0.0.1:3000";
+const URL = import.meta.env.VITE_API_URLS ?? "http://127.0.0.1:3000";
 export const socket = socketClient(URL);
